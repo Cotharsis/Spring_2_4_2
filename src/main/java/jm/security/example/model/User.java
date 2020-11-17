b6668784d425f73b459d4a4e1ca5715edc3726a9
Cotharsis <valeev-p@mail.ru>
2020-11-17 20:57:32 +0300

    Добавил параметры юзера

--- src/main/java/jm/security/example/model/User.java
+++ src/main/java/jm/security/example/model/User.java
@@ -8,11 +8,30 @@ import java.util.Set;
 
 // Для того, чтобы в дальнейшим использовать класс User в Spring Security, он должен реализовывать интерфейс UserDetails.
 // UserDetails можно представить, как адаптер между БД пользователей и тем что требуется Spring Security внутри SecurityContextHolder
+//@Entity
+//@Table(name = "users")
 public class User implements UserDetails {
+    private String name;
+    private int year;
+    private String address;
+//    @Id
+//    @Column(name = "id")
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username; // уникальное значение
     private String password;
     private Set<Role> roles;
+
+//    public User(String name, int year, String address, Long id, String username,
+//                String password, Set<Role> roles) {
+//        this.name = name;
+//        this.year = year;
+//        this.address = address;
+//        this.id = id;
+//        this.username = username;
+//        this.password = password;
+//        this.roles = roles;
+//    }
 
     public User() {
 
@@ -82,5 +101,29 @@ public class User implements UserDetails {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
